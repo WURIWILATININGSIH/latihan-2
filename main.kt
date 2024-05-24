be3bb2ee7d298870801b570139316473bbc5dd88
WURI WILATININGSIH 121140167 <117991213+WURIWILATININGSIH@users.noreply.github.com>
2024-05-24 21:53:38 +0700

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,11 @@
+package com.example.yourapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ImageBackgroundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_background)
+    }
+}
